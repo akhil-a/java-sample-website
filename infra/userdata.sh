0579df311016b978e89efc389a69c0ac94e688b3
Akhil A <akhil10anil@gmail.com>
2025-07-18 14:46:00 +0530

    added terraform files

--- infra/userdata.sh
+++ infra/userdata.sh
@@ -3,3 +3,6 @@
 sudo yum install java-17
 sudo yum install docker java-17 maven git -y
 sudo systemctl start docker
+
+docker pull docker pull akhilanilkumar10/java-web-app
+docker run docker pull -p 8080:8080 akhilanilkumar10/java-web-app
